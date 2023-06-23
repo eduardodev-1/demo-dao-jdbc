b9089bec3eeefbcccb96c49e67a64117a0c6177b
eduardodev-1 <122590889+eduardodev-1@users.noreply.github.com>
2023-06-23 17:11:23 -0300

    insert implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import application.dao.DaoFactory;
@@ -16,6 +17,7 @@ public class App {
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		System.out.println();
 		
 		System.out.println("=== TEST 2: seller findByDepartment ===");
 		Department department = new Department(2);
@@ -23,11 +25,19 @@ public class App {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println();
 		
 		System.out.println("=== TEST 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println();
+		
+		System.out.println("=== TEST 4: seller seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		System.out.println();
 	}
 }
--- src/application/dao/implement/SellerDaoJDBC.java
+++ src/application/dao/implement/SellerDaoJDBC.java
@@ -1,9 +1,11 @@
 package application.dao.implement;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,9 +28,59 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-		
+	public void insert(Seller seller) {
+		PreparedStatement st = null;
+		Statement st1 = null;
+		try {
+			String sql = "INSERT INTO seller "
+					+ "(Id, Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?, ?)";
+			String sqlNoId = "INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)";
+			
+			
+			Integer nextFreeId = verifyFirstFreeId();
+			if (nextFreeId != null) {
+				st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+				st.setInt(1, nextFreeId);
+				st.setString(2, seller.getName());
+				st.setString(3, seller.getEmail());
+				st.setDate(4, new Date(seller.getBirthDate().getTime()));
+				st.setDouble(5, seller.getBaseSalary());
+				st.setInt(6, seller.getDepartment().getId());
+			}
+			else {
+				st1 = conn.createStatement();
+				st1.execute("ALTER TABLE seller AUTO_INCREMENT = 1");
+				DB.closeStatement(st1);
+				st = conn.prepareStatement(sqlNoId, Statement.RETURN_GENERATED_KEYS);
+				st.setString(1, seller.getName());
+				st.setString(2, seller.getEmail());
+				st.setDate(3, new Date(seller.getBirthDate().getTime()));
+				st.setDouble(4, seller.getBaseSalary());
+				st.setInt(5, seller.getDepartment().getId());
+			}
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				rs.first();
+				int id = rs.getInt(1);
+				seller.setId(id);
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage())
+;		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -167,5 +219,32 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
+	}
+	private Integer verifyFirstFreeId() throws SQLException {
+		Statement st1 = null;
+		Statement st2 = null;
+		ResultSet rs1 = null;
+		ResultSet rs2 = null;
+		Integer nextFreeId = null;
+		
+		st1 = conn.createStatement();
+		st2 = conn.createStatement();
+
+		rs1 = st1.executeQuery("select * from seller");
+
+		while (rs1.next()) {
+			int verificacao = rs1.getInt("Id") + 1;
+			rs2 = st2.executeQuery("select * from seller where Id = '" + verificacao + "'");
+			if (!rs2.next()) {
+				nextFreeId = verificacao;
+				return nextFreeId;
+			}
+		}
+		DB.closeResultSet(rs1);
+		DB.closeResultSet(rs2);
+		DB.closeStatement(st1);
+		DB.closeStatement(st2);
+		return null;
+		
 	}
 }
