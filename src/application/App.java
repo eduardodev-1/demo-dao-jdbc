c570df2823bd5a4d2171a237d6bce6a3ac516231
eduardodev-1 <122590889+eduardodev-1@users.noreply.github.com>
2023-06-23 01:29:25 -0300

    Seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import application.entities.Department;
+import application.entities.Seller;
 
 public class App {
 
@@ -8,5 +11,9 @@ public class App {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 }
